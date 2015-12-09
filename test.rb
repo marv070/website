c276c9cbd22afb7ef2cb449218291949da285fdc
Marvin Laucher <marv070@gmail.com>
2015-12-09 16:12:58 -0500

    adding test to check for correct erb call up

--- test.rb
+++ test.rb
@@ -11,21 +11,33 @@ class App_test < Minitest::Test
     Sinatra::Application
   end
 
-  # def test_for_hello_world
-  #   get '/'
-  #   assert last_response.ok?
-  #   assert_equal 'hello world', last_response.body
-  # end
 
   def test_it_redirects_to_erb_index
     get '/'
     assert last_response.ok?
     assert last_response.body.include?('slideshowimages("mm.png","rules.png","ttt.png")')
     assert last_response.body.include?('slideshowlinks("http://localhost:4567/minedminds","http://localhost:4567/rules","http://localhost:4567/playgame","","")')
+    assert last_response.body.include?('class="active"href="http://localhost:4567/">Home')
   end
 
 
+  def test_it_redirects_to_rules_erb
+    get '/rules'
+    assert last_response.ok?
+    assert last_response.body.include?('IMG SRC="rules.png"')
+    assert last_response.body.include?('"https://en.wikipedia.org/wiki/Tic-tac-toe#Strategy"')
+    assert last_response.body.include?('class="active"href="http://localhost:4567/rules">Rules')
+    assert_equal "http://example.org/rules",last_request.url
+  end
 
+  
+  def test_it_redirects_to_playgame_erb
+    get '/playgame'
+    assert last_response.ok?
+    assert_equal "http://example.org/playgame",last_request.url
+    assert last_response.body.include?('class="active"href="http://localhost:4567/playgame">Play Game')
+    assert last_response.body.include?('IMG SRC="ttt.png"')
+  end
 
 
 end
